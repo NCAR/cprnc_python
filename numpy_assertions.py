6a5db94e97f86906551fe9a7ed6fafa9b63e885d
Bill Sacks <sacks@ucar.edu>
2015-12-23 16:48:02 -0700

    Add more informative prints in assertion message

--- numpy_assertions.py
+++ numpy_assertions.py
@@ -5,12 +5,21 @@ class NumpyAssertions:
     """This class provides custom assertions on numpy arrays"""
 
     def assertArraysEqual(self, arr1, arr2):
-        masks_equal = np.array_equal(
-            ma.getmaskarray(arr1),
-            ma.getmaskarray(arr2))
-        vals_equal = ma.allequal(arr1, arr2)
+        mask1 = ma.getmaskarray(arr1)
+        mask2 = ma.getmaskarray(arr2)
+        masks_equal = np.array_equal(mask1, mask2)
         if (not masks_equal):
-            raise AssertionError("Masks differ")
+            msg = "Masks differ:\n" + \
+              str(mask1) + " != " + str(mask2) + "\n" + \
+              "Arrays are: \n" + \
+              str(arr1) + "\n" + \
+              str(arr2)
+
+            raise AssertionError(msg)
+
+        vals_equal = ma.allequal(arr1, arr2)
         if (not vals_equal):
-            raise AssertionError("Values differ")
+            msg = "Values differ:\n" + \
+              str(arr1) + " != " + str(arr2)
+            raise AssertionError(msg)
 
