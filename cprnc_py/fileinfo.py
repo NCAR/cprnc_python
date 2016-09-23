3e1d772da97e616857d1ba89a641bc9368865faa
Michael Deakin <mdeakin@sandia.gov>
2016-09-23 17:25:24 -0600

    Implemented FileInfo._add_varinfo

--- cprnc_py/fileinfo.py
+++ cprnc_py/fileinfo.py
@@ -1,6 +1,6 @@
 
 from __future__ import print_function
-from multiprocessing import Pool
+from multiprocessing import Process, Queue
 from cprnc_py.multiprocessing_fake import PoolFake
 from cprnc_py.varinfo import (VarInfo, VarInfoNonNumeric)
 
@@ -26,7 +26,7 @@ class FileInfo(object):
          - str(finfo)
     """
 
-    def __init__(self, file, separate_dim="time", nprocs=None):
+    def __init__(self, ncfile, separate_dim="time", nprocs=None):
         """Create a FileInfo object.
 
         Arguments:
@@ -42,5 +42,45 @@ class FileInfo(object):
             nprocs > 1 means to use multiple tasks with the multiprocessing
             package
         """
+        self._file = ncfile
+        self._nprocs = nprocs
+        if (separate_dim and separate_dim in ncfile.get_dimlist()):
+            self._add_separated_varinfo(separate_dim)
+        else:
+            self._add_varinfo()
 
-        
+    def __str__(self):
+        mystr = ""
+        numNonNumeric = 0
+        for var in self.varlist:
+            mystr += str(var) + "\n\n"
+            if type(var) == VarInfoNonNumeric:
+                numNonNumeric += 1
+        mystr += ("SUMMARY of cprnc_singlefile\n" +
+                  "A total of {0:6d} fields were analyzed\n" +
+                  "        of which {1:6d} were " +
+                  "non-numeric").format(len(self.varlist), numNonNumeric)
+        return mystr
+
+    def _add_varinfo():
+        q = Queue()
+        vnamelist = self._file.get_varlist()
+        procs = [Process(target=_create_varinfo_wrapper,
+                         args=(self._file, varname, q, None))
+                 for varname in vnamelist]
+        for p in procs:
+            p.start()
+        self.varlist = []
+        for p in procs:
+            p.join()
+            self.varlist.append(q.get())
+
+def _create_varinfo_wrapper(f, varname, q, dim_indices={}):
+    if not f.has_variable(varname):
+        q.put(None)
+        return
+    if not f.is_var_numeric(varname):
+        q.put(VarInfoNonNumeric(varname))
+        return
+    q.put(VarInfo(f.get_vardata(varname, dim_indices)))
+    return
